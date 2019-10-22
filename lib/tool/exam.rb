fe3c58f297b3cc36bf19a75e927636c149207d07
David <dvarrui@protonmail.com>
2019-10-22 12:30:28 +0100

    U

--- lib/tool/exam.rb
+++ lib/tool/exam.rb
@@ -1,8 +1,8 @@
 
 require_relative '../export/md_exporter'
 
-module CreateExam
-  def self.run(id, filename, questions, indexes)
+module Exam
+  def self.create(id, filename, questions, indexes)
     puts Rainbow("[#{format('%02d', id)}]").bright
     MDExporter.run("#{format('%02d',id)}-#{filename}",
                    questions, indexes)
