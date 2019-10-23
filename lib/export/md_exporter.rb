54456e01665b5ca1fe1eb313e7c44dec48b64e33
David <dvarrui@protonmail.com>
2019-10-23 12:15:21 +0100

    U formatter_qexam

--- lib/export/md_exporter.rb
+++ lib/export/md_exporter.rb
@@ -15,9 +15,29 @@ module MDExporter
     questions.each_with_index do |q,i|
       f.write("---\n")
       f.write("**Question #{(i+1)}**\n\n")
-      f.write("#{q[:text]}\n\n")
+      f.write(formatter_qexam(q))
     end
     f.close
+  end
+
+  def self.formatter_qexam(question)
+    if question[:type] == :choice
+      t = "#{question[:text]}\n\nChoose option:\n"
+      question[:options].each_with_index do |v,i|
+        t += "#{(i+1)}. #{v}\n"
+      end
+      return t
+    elsif question[:type] == :match
+        t = "#{question[:text]}\n\nAssociate columns:\n" +
+            "| Column A | Column B |\n" +
+            "| -------- | -------- |\n"
+        question[:answer].each do |v|
+          t += "| #{v[0]} | #{v[1]} |\n"
+        end
+        return t
+    elsif question[:type] == :short
+      return "#{question[:text]}\n\nWrite your answer:\n\n"
+    end
   end
 
   def self.export_solu(filename, questions)
