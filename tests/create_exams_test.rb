aa9ac056ce18aef085270536a0aa48f156c7a654
David <dvarrui@protonmail.com>
2019-10-24 11:44:18 +0100

    N test CreateExam

---
+++ tests/create_exams_test.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require 'minitest/autorun'
+require_relative '../lib/tool/create_exams'
+
+# Test Application singleton
+class ApplicationTest < Minitest::Test
+  def test_proces_input_params
+    app = Application.instance
+    app.reset
+
+    filepath = 'foo.yaml'
+    options = { op1: 'op1', op2: 'op2'}
+    params = { param1: 'param1', param2: 'param2'}
+    CreateExams.process_input_params(filepath, options, params)
+    assert_equal filepath, app.get(:filepath)
+    assert_equal options[:op1], app.get(:op1)
+    assert_equal options[:op2], app.get(:op2)
+    assert_equal params[:param1], app.get(:param1)
+    assert_equal params[:param2], app.get(:param2)
+  end
+end
