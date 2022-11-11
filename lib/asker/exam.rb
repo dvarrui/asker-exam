77b175c19b36ad9a82ef10b079765a1ef96cd99a
dvarrui <dvarrui@protonmail.com>
2022-11-11 23:20:05 +0000

    [E] .

--- lib/asker/exam.rb
+++ lib/asker/exam.rb
@@ -3,7 +3,7 @@ require_relative 'exam/create_exams'
 
 module Asker
   module Exam
-    def self.create_exams(filename, options)
+    def self.create(filename, options)
       CreateExams.call(filename, options)
     end
   end
--- lib/asker/exam/cli.rb
+++ lib/asker/exam/cli.rb
@@ -3,34 +3,30 @@ require 'rainbow'
 require_relative '../exam'
 require_relative 'version'
 
-module Asker
-  module Exam
-    class CLI < Thor
-      map ['h', '-h', '--help'] => 'help'
+class CLI < Thor
+  map ['h', '-h', '--help'] => 'help'
 
-      map ['v', '-v', '--version'] => 'version'
-      desc 'version', 'show the program version'
-      def version
-        puts "#{Version::NAME} (version #{Version::VERSION})"
-      end
+  map ['v', '-v', '--version'] => 'version'
+  desc 'version', 'show the program version'
+  def version
+    puts "#{Version::NAME} (version #{Version::VERSION})"
+  end
 
-      map ['f', '-f', '--file'] => 'file'
-      desc 'file NAME', 'Build exams, from YAML input file.'
-      option :color, type: :boolean
-      long_desc <<-LONGDESC
-      Create exams, from questions input file (YAML format).
-      LONGDESC
+  map ['f', '-f', '--file'] => 'file'
+  desc 'file NAME', 'Build exams, from YAML input file.'
+  option :color, type: :boolean
+  long_desc <<-LONGDESC
+  Create exams, from questions input file (YAML format).
+  LONGDESC
 
-      ##
-      # Read questions from filename and creates exams.
-      # @param filename [String] File path to YAML questions file.
-      def file(filename)
-        create_exams(filename, options)
-      end
+  ##
+  # Read questions from filename and creates exams.
+  # @param filename [String] File path to YAML questions file.
+  def file(filename)
+    Asker::Exam.create(filename, options)
+  end
 
-      def method_missing(method, *_args, &_block)
-        file(method.to_s)
-      end
-    end
+  def method_missing(method, *_args, &_block)
+    file(method.to_s)
   end
 end
