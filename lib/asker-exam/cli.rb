a15df86ff6f1a47004f42debcb8183a29aa25819
David Vargas <dvarrui@protonmail.com>
2022-11-11 21:05:13 +0000

    [U] .

--- lib/asker-exam/cli.rb
+++ lib/asker-exam/cli.rb
@@ -1,4 +1,6 @@
 require 'thor'
+require 'rainbow'
+require_relative '../tool/create_exams'
 require_relative 'version'
 
 module AskerExam
@@ -10,5 +12,24 @@ module AskerExam
     def version
       puts "#{Version::NAME} (version #{Version::VERSION})"
     end
+  end
+
+  map ['f', '-f', '--file'] => 'file'
+  desc 'file NAME', 'Build exams, from YAML input file.'
+  option :color, type: :boolean
+  long_desc <<-LONGDESC
+  Create exams, from questions input file (YAML format).
+  LONGDESC
+
+  ##
+  # Read questions from YAML filename and creates exams.
+  # @param filename [String] File path to YAML questions file.
+  def file(filename)
+    Rainbow.enabled = false if options['color'] == false
+    CreateExams.run(filename, options)
+  end
+
+  def method_missing(method, *_args, &_block)
+    file(method.to_s)
   end
 end
