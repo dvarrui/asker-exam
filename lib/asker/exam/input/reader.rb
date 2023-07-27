8c4396cfe57a0a373fcfef222485a309faa6b2e9
dvarrui <dvarrui@protonmail.com>
2023-07-27 23:02:28 +0100

    [F] some problems

--- lib/asker/exam/input/reader.rb
+++ lib/asker/exam/input/reader.rb
@@ -4,7 +4,7 @@ module InputReader
   def self.read(filename)
     # YAML.load(File.read(filename))
     YAML.safe_load(
-      File.read(filepath),
+      File.read(filename),
       permitted_classes: [Array, Hash, Symbol]
     )
   end
