34f00c3ff6d9413cd4d0205995aabe41114c44f0
dvarrui <dvarrui@protonmail.com>
2023-08-05 19:56:24 +0100

    [U] export match questions 2 txt

--- lib/asker/exam/export/export2txt.rb
+++ lib/asker/exam/export/export2txt.rb
@@ -5,6 +5,7 @@ class Export2txt
     filename1 = format("%s-exam-%02d.txt", name, id)
     filename2 = format("%s-solu-%02d.txt", name, id)
     puts "==> asker-exam: #{filename1}, #{filename2}"
+    questions.shuffle!
     export_exam(filename1, questions)
     export_solu(filename2, questions)
   end
@@ -14,9 +15,9 @@ class Export2txt
   def export_exam(filename, questions)
     file = File.open(File.join("output", filename), "w")
     file.write("-" * 50 + "\n")
-    file.write("AskerExam : #{Asker::Exam::VERSION}\n")
-    file.write("Filename  : #{filename}\n")
-    file.write("Datetime  : #{Time.new}\n")
+    file.write("  AskerExam : #{Asker::Exam::VERSION}\n")
+    file.write("  Filename  : #{filename}\n")
+    file.write("  Datetime  : #{Time.new}\n")
     file.write("-" * 50 + "\n\n")
     export_questions(file, questions)
     file.close
@@ -38,13 +39,32 @@ class Export2txt
       file.write(format("%2d) ", index + 1))
       if question[:type] == :boolean
         file.write("#{question[:text]}\n")
-        file.write("    (True/False?)\n")
+        file.write("    (True/False)\n")
       elsif question[:type] == :choice
           file.write("#{question[:text]}\n")
           question[:options].each_with_index do |option, index|
             letter = %w[a b c d][index]
             file.write("    #{letter}) #{option}\n")
           end
+          file.write("    (Choose option)\n")
+        elsif %i[match ddmatch].include? question[:type]
+            file.write("#{question[:text]}\n")
+            list1 = []
+            list2 = []
+            question[:answer].each do
+              list1 << _1
+              list2 << _2
+            end
+            file.write("    List1: #{list1.shuffle}\n")
+            file.write("    List2: #{list2.shuffle}\n")
+            file.write("    (Match)\n")
+      elsif question[:type] == :ordering
+          file.write("#{question[:type]}\n")
+          file.write("   #{question[:text]}\n")
+          question[:answer].shuffle.each do
+            file.write(" " * 4 + "#{_1}\n")
+          end
+          file.write("    (Order)\n")
       elsif question[:type] == :short
         file.write("#{question[:text]}\n")
         file.write("    (Write your answer)\n")
@@ -68,6 +88,11 @@ class Export2txt
         letter_index = question[:options].index question[:answer]
         letter = %w[a b c d][letter_index]
         file.write("  choice: #{letter} -> #{question[:answer]}\n")
+      elsif question[:type] == :ordering
+          file.write("#{question[:type].to_s.rjust(8)}:\n")
+          question[:answer].each do
+            file.write(" " * 14 + "#{_1}\n")
+          end
       else
         file.write("#{question[:type].to_s.rjust(8)}: #{question[:answer]}\n")
       end
